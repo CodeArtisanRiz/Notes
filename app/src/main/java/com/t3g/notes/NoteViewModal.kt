bf399843a48f9f254e8ae3d6860fa34616a3967a
H M Rizwan Mazumder <42119262+xtremist96@users.noreply.github.com>
2023-06-13 01:55:40 +0530

    Initial commit Create Project Add Dependencies, plugin, pakagingOptions Update SDK to 34 Added colours Created: Note(class), NotesDao(interface), NoteDatabase(abstract class), NoteRepository(class), NoteViewModal(class)

---
+++ app/src/main/java/com/t3g/notes/NoteViewModal.kt
@@ -0,0 +1,9 @@
+package com.t3g.notes
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class NoteViewModal(application: Application) : AndroidViewModel(application){
+
+
+}
