7ea8d737d2ce214df1e26f91f9b501b48576cf21
H M Rizwan Mazumder <42119262+xtremist96@users.noreply.github.com>
2023-06-19 15:24:15 +0530

    Updated : UI - activity_main Created : Funtions in MainActivity, note_rv_item

--- app/src/main/java/com/t3g/notes/MainActivity.kt
+++ app/src/main/java/com/t3g/notes/MainActivity.kt
@@ -2,10 +2,20 @@ package com.t3g.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var notesRV: RecyclerView
+    lateinit var addFAB: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notesRV = findViewById(R.id.RVNotes)
+        addFAB = findViewById(R.id.FABAddNote)
+
+
     }
 } 
